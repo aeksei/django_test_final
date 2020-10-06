7a51c33b2a4f2aa91a92f48c5707ec8e1d429784
aeksei <aeksei94@gmail.com>
2020-10-06 19:00:04 +0300

    controller

--- test_proj/urls.py
+++ test_proj/urls.py
@@ -13,10 +13,29 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import sys
+from datetime import datetime
+
 from django.contrib import admin
 from django.urls import path
 
+from django.http import HttpResponse
+
+
+def _get_info(request):
+    html = f"""<html><body>{sys.platform} | {sys.version}</body></html>"""
+    return HttpResponse(html)
+
+
+def _get_date(request):
+    html = f"""{datetime.now()}"""
+    return HttpResponse(html)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('info/', _get_info),
+    path('date/', _get_date),
+
 ]
 
