746324af5809f42eb8033fc9b22e5f7887372378
aeksei <aeksei94@gmail.com>
2020-10-06 20:43:55 +0300

    split view, urls (MVT)

--- blog/urls.py
+++ blog/urls.py
@@ -1,55 +1,8 @@
-import json
-
-from django.urls import path, reverse
-from django.http import HttpResponse, JsonResponse
-from django.shortcuts import redirect
-
-
-blogs = {
-    2019: [
-        {
-            'author': 'Пушкин',
-            'date': '03.06.2019',
-            'like': 20
-        },
-        {
-            'author': 'Толстой',
-            'date': '27.07.2019',
-            'like': 100
-        }
-    ],
-    2020: [
-        {
-            'author': 'Лермонтов',
-            'date': '07.10.2020',
-            'like': 73
-        }
-    ],
-}
-
-
-def _get_blogs_view(request):
-    html = ""
-    for year in blogs:
-        html += f"{year}<br>"
-
-    return HttpResponse(html)
-
-
-def _get_blog_year_view(request, year):
-    url = reverse('article', args=(year, 0))
-    return redirect(url)
-
-
-def _get_article(request, year, pk=None):
-    try:
-        return JsonResponse(blogs[year][pk])
-    except IndexError:
-        return HttpResponse("<h1>Такой статьи нет</h1>")
-
+from django.urls import path
+from .views import *
 
 urlpatterns = [
-    path('blogs_view/', _get_blogs_view),
-    path('blogs_view/<int:year>', _get_blog_year_view),
-    path('blogs_view/<int:year>/<int:pk>', _get_article, name="article"),
+    path('blogs_view/', get_blogs_view),
+    path('blogs_view/<int:year>', get_blog_year_view),
+    path('blogs_view/<int:year>/<int:pk>', get_article, name="article"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+
+blogs = {
+    2019: [
+        {
+            'author': 'Пушкин',
+            'date': '03.06.2019',
+            'like': 20
+        },
+        {
+            'author': 'Толстой',
+            'date': '27.07.2019',
+            'like': 100
+        }
+    ],
+    2020: [
+        {
+            'author': 'Лермонтов',
+            'date': '07.10.2020',
+            'like': 73
+        }
+    ],
+}
+
+
+def get_blogs_view(request):
+    # todo url redirect
+    html = ""
+    for year in blogs:
+        html += f"{year}<br>"
+
+    return HttpResponse(html)
+
+
+def get_blog_year_view(request, year):
+    url = reverse('article', args=(year, 0))
+    return redirect(url)
+
+
+def get_article(request, year, pk=None):
+    try:
+        return JsonResponse(blogs[year][pk])
+    except IndexError:
+        return HttpResponse("<h1>Такой статьи нет</h1>")
