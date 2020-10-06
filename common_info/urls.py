904de11ee508dd40974316b4a21801e00614acce
aeksei <aeksei94@gmail.com>
2020-10-06 19:33:42 +0300

    new app

---
+++ common_info/urls.py
@@ -0,0 +1,26 @@
+import sys
+from datetime import datetime
+
+from django.urls import path
+from django.http import HttpResponse, JsonResponse
+
+
+def _get_info(request):
+    # html = f"""<html><body>{sys.platform} | {sys.version}</body></html>"""
+    json_ = {
+        'platform': sys.platform,
+        'python': sys.version
+    }
+
+    return JsonResponse(json_)
+
+
+def _get_date(request):
+    html = f"""{datetime.now()}"""
+    return HttpResponse(html)
+
+
+urlpatterns = [
+    path('info/', _get_info),
+    path('date/', _get_date),
+]
--- test_proj/urls.py
+++ test_proj/urls.py
@@ -13,36 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import sys
-from datetime import datetime
-
 from django.contrib import admin
-from django.urls import path
-
-from django.http import HttpResponse, JsonResponse
-
-
-def _get_info(request):
-    # html = f"""<html><body>{sys.platform} | {sys.version}</body></html>"""
-    json_ = {
-        'platform': sys.platform,
-        'python': sys.version
-    }
-
-    return JsonResponse(json_)
-
-
-def _get_date(request):
-    html = f"""{datetime.now()}"""
-    return HttpResponse(html)
-
-
+from django.urls import path, include
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('info/', _get_info),
-    path('date/', _get_date),
+    path('', include('common_info.urls')),
 
 ]
-
