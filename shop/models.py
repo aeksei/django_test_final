4d162c06d0709ddd34f452ac83146458dd642d73
aeksei <aeksei94@gmail.com>
2020-10-13 20:11:50 +0300

    init shop orm

---
+++ shop/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+
+# Create your models here.
+
+
+class User(models.Model):
+    SEX = (
+        ('1', 'Мужчина'),
+        ('0', 'Женщина')
+
+    )
+    # id = models.AutoField()  # integer
+    email = models.EmailField(primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+    phone_number = models.CharField(max_length=12, null=True, blank=True)
+    sex = models.CharField(max_length=1, choices=SEX, null=True, blank=True)
+
+    city = models.CharField(max_length=20, editable=False)
+    age = models.IntegerField(default=18)
+
+    def __str__(self):
+        return f"{self.first_name.title()} {self.last_name.title()}"
+
+
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=20, unique=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Cart(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"{self.user} - {self.product}"
+
+    class Meta:
+        unique_together = (('user', 'product'), )
--- test_proj/settings.py
+++ test_proj/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'common_info',
+    'shop',
 ]
 
 MIDDLEWARE = [
