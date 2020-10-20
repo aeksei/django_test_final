9ad070172a271ef390f05cbc95ad989a75cd7344
aeksei <aeksei94@gmail.com>
2020-10-20 19:52:40 +0300

    add var

--- shop_1/views.py
+++ shop_1/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render
 from django.views import View
 
-# Create your views here.
+from .settings.info import INFO
 
+# Create your views here.
 
 class IndexView(View):
 
     def get(self, request):
-        return render(request, 'shop_1/index.html')
+
+        contex = {}
+        contex.update(INFO)
+        return render(request, 'shop_1/index.html', contex)
 
 
 class ShopView(View):
+
     def get(self, request):
-        return render(request, 'shop_1/shop.html')
+        contex = {}
+        contex.update(INFO)
+        return render(request, 'shop_1/shop.html', contex)
